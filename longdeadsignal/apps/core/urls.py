b90b4668da83e4a580205224e18ddf226e5d944d
Marcus Whybrow <marcus@marcuswhybrow.net>
2011-03-11 23:31:57 +0000

    Added /photos/ page which displays all of the flickr photos.

--- longdeadsignal/apps/core/urls.py
+++ longdeadsignal/apps/core/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls.defaults import patterns, include, url
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from syncr.flickr.models import Photo
 
 urlpatterns = patterns('longdeadsignal.apps.core.views',
     # Examples:
     # url(r'^$', 'longdeadsignal.views.home', name='home'),
     # url(r'^longdeadsignal/', include('longdeadsignal.foo.urls')),
     
+    url(r'^photos/$', ListView.as_view(model=Photo), name='photo_list'),
     url(r'^$', TemplateView.as_view(template_name='core/index.html'), name='index'),
 )
