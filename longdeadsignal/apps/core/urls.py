252508b792d8eecc341db12e880fcd0b7a8bd16d
Marcus Whybrow <marcus@marcuswhybrow.net>
2011-03-12 03:17:16 +0000

    Added the Biography page.

--- longdeadsignal/apps/core/urls.py
+++ longdeadsignal/apps/core/urls.py
@@ -9,6 +9,7 @@ urlpatterns = patterns('longdeadsignal.apps.core.views',
     # url(r'^$', 'longdeadsignal.views.home', name='home'),
     # url(r'^longdeadsignal/', include('longdeadsignal.foo.urls')),
     
+    url(r'^bio/$', TemplateView.as_view(template_name='core/biography.html'), name='biography'),
     url(r'^photos/$', ListView.as_view(model=Photo), name='photo_list'),
     url(r'^videos/$', ListView.as_view(model=Video), name='video_list'),
     url(r'^videos/(?P<slug>[^/]+)/$', DetailView.as_view(model=Video, slug_field='video_id'), name='video_detail'),
