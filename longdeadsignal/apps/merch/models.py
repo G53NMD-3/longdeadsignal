b9ad24d8fc0c9711795f6da6252f2e946d689ba1
Marcus Whybrow <marcus@marcuswhybrow.net>
2011-03-23 12:21:24 +0000

    Made the Merch slug field automatic, unique and indexed.

--- longdeadsignal/apps/merch/models.py
+++ longdeadsignal/apps/merch/models.py
@@ -1,6 +1,16 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 class Merch(models.Model):
     title = models.CharField(max_length=128)
-    slug = models.SlugField(max_length=128)
+    slug = models.SlugField(db_index=True, editable=False, unique=True)
     price = models.PositiveIntegerField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    
+    def __unicode__(self):
+        return u'%s' % self.title
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Merch, self).save(*args, **kwargs)
+    
