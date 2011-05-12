133d171111180d02c97026d97dbd56111e3f5a03
Marcus Whybrow <marcus@marcuswhybrow.net>
2011-05-12 20:29:06 +0100

    Styled the pagination stuff a bit.

--- longdeadsignal/apps/news/settings.py
+++ longdeadsignal/apps/news/settings.py
@@ -1,4 +1,4 @@
 from django.conf import settings
 
-PAGINATE_BY = getattr(settings, 'NEWS_PAGINATE_BY', 10)
+PAGINATE_BY = getattr(settings, 'NEWS_PAGINATE_BY', 1)
 POST_FEED_ITEM_COUNT = getattr(settings, 'NEWS_POST_FEED_ITEM_COUNT', 10)
